b8311a93670e3d6545ffae47f9cc5722d7ad0b06
William Lewis <wrlewis@ucsd.edu>
2015-02-27 23:14:49 +0000

    initial analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,11 +12,11 @@ function initializePage() {
 	// add any functionality and listeners you want here
 
   $(".version_a").click(function(){
-    //add your Woopra tracking code for version A's like button click event
+    woopra.track("a_version_like_click");
   })
 
   $(".version_b").click(function(){
-    //add your Woopra tracking code for version A's like button click event
+    woopra.track("b_version_like_click");
   })
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var projects = require("../projects.json")
+var projects = require("../projects.json");
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
   // Here you should implement your random landing page rendering functionality
-  res.render("index", projects);
+    var random_num = Math.random();
+
+  if(random_num > 0.5){
+    res.render("index", projects);
+  }
+  else{
+    res.render("index_alternate", projects);
+  }
 };
